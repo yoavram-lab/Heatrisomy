7faab0318fbd9d6f2bb861fae695562a81fa7c10
Martin <martin.pontz@gmail.com>
2022-06-30 12:20:47 +0200

    checked and cleaned the simulations file

--- src/models/singlelocusmodelextended.py
+++ src/models/singlelocusmodelextended.py
@@ -12,6 +12,8 @@ class SingleLocusModelExt:
     # w1 is 2n+1 fitness
     # w2 is 2n+1* fitness
     # w3 is 2n* fitness
+    
+    #This method simulates follows the 4 states and records their frequency for every generation
     def run_simulations(self, N, mutation_rate, aneuploidy_rate, aneuploidy_loss_rate, w1, w2, w3, repetitions=1000, fixation=0.95, max_gen=2500, seed=3, clonal_intf=True):
         curr_rand_state = np.random.get_state()
         np.random.seed(seed)
@@ -19,7 +21,8 @@ class SingleLocusModelExt:
         #0 wt
         #1 tri
         #2 tri+
-        #3 +
+        #3 + (from tri+)
+        #4 + (from wt)
         Mt = np.diag(np.repeat(0.,5))
         Mt[0][1] = aneuploidy_rate
         Mt[1][2] = mutation_rate
@@ -32,8 +35,8 @@ class SingleLocusModelExt:
         return (times, p, non_aneu_fix)   
 
     
-    #have side effect on M, but not harmfull, can run twice simulation
-    def _simulation(self, N, w, Mt ,repetitions=1000, fixation=0.95, max_gen=2500,clonal_intf=True):
+    
+    def _simulation(self, N, w, Mt ,repetitions=1000, fixation=0.95, max_gen=2500, clonal_intf=True):
         assert N > 0
         N = np.uint64(N)
         L = len(w)
@@ -61,14 +64,12 @@ class SingleLocusModelExt:
         # which columns to update
         update = np.array([True] * repetitions)
         directmut = np.array([True] * repetitions)
-        #clonal_int=np.array([False]*repetitions)
         combfix=np.array([True] * repetitions)
-        fixbool=np.array([False]*repetitions)
+
         # follow time
         T = np.zeros(repetitions)
         t = 0
         # follow population mean fitness
-        W = []
         P = []
         E=Et
 
@@ -79,10 +80,8 @@ class SingleLocusModelExt:
             t += 1
             T[update] = t        
             p = n/N  # counts to frequencies
-    #         W.append(w.reshape(1, L) @ p)  # mean fitness
             P.append(p)
             p = E @ p  # natural selection + mutation   
-            #p[-1,clonal_int]=0
             p /= p.sum(axis=0)  # mean fitness
             for j in update.nonzero()[0]:
                 # random genetic drift
@@ -91,22 +90,21 @@ class SingleLocusModelExt:
             if clonal_intf==False:
                 if (n[1,update]+n[-1,update])>=1:
                     E=Enotr
-            #clonal_int[update]=(n[1,update]+n[-1,update])>=1
             update = (n[-2,:] < N*fixation)  # fixation of fittest genotype with aneuploidy
-            update = update*directmut
+            update = update*directmut     # get all fixed repetitions into update
             combfix[update] = (n[-1,update]+n[-2,update] < N*fixation)  # fixation of fittest genotype as a combination
-            update = update*combfix
+            update = update*combfix    # get all fixed repetitions into update
+            
         #last generation update
-        
         p = n/N 
         P.append(p)
-        nona=(~directmut).nonzero()[0]
-        combfix=(~combfix).nonzero()[0]
+        nona=(~directmut).nonzero()[0] #an array that stores the indices of the repetition where the 2n* is fixed through dircet mutations
+        combfix=(~combfix).nonzero()[0] #an array that stores the indices of the repetition where the 2n* is fixed as a combination
 
         return T, P, (nona, combfix)
         #return T, P, nona
     
-    # simulations only with waiting time as return
+    # simulations only with waiting times and the frequency of 2n* through both trajectories (if fix_freq=True) as return 
     def run_simulations_time(self, N, mutation_rate, aneuploidy_rate, aneuploidy_loss_rate, w1, w2, w3, repetitions=1000, max_gen=2500, fixation=0.95, seed=5,clonal_intf=True, fix_frequ=False):
         curr_rand_state = np.random.get_state()
         np.random.seed(seed)
@@ -114,7 +112,8 @@ class SingleLocusModelExt:
         #0 wt
         #1 tri
         #2 tri+
-        #3 +
+        #3 + (from tri+)
+        #4 + (from wt)
         M = np.diag(np.repeat(0.,5))
         M[0][1] = aneuploidy_rate
         M[1][2] = mutation_rate
@@ -160,14 +159,12 @@ class SingleLocusModelExt:
         update = np.array([True] * repetitions)
         directmut = np.array([True] * repetitions)
         combfix=np.array([True] * repetitions)
-        clonal_int=np.array([False]*repetitions)
-        fixbool=np.array([False]*repetitions)
+
         # follow time
         T = np.zeros(repetitions)
         fixT=np.zeros(repetitions)
         t = 0
-        # follow population mean fitness
-        W = []
+
         P = []
 
         while update.any():
@@ -177,14 +174,12 @@ class SingleLocusModelExt:
             t += 1
             T[update] = t   
             p = n/N  # counts to frequencies
-            #W.append(w.reshape(1, L) @ p)  # mean fitness
-            #P.append(p)
             if clonal_intf==False:
                 p[:,~clonal_int] = E @ p[:,~clonal_int]  # natural selection + mutation 
                 p[:,clonal_int] = Enotr @ p[:,clonal_int]     
             else:
                 p = E @ p
-            #p[-1,clonal_int]=0
+
             p /= p.sum(axis=0)  # mean fitness
             for j in update.nonzero()[0]:
                 # random genetic drift
@@ -192,16 +187,15 @@ class SingleLocusModelExt:
             directmut[update] = (n[-1,update] < N*fixation)  # fixation of fittest genotype directly through mutation
             if clonal_intf==False:
                 clonal_int[update]=(n[1,update]+n[-1,update])>=1
-            #clonal_int[update]=np.logical_and((n[1,update]+n[2,update]>N*clonal_block_perc),(n[-1,update]<N*clonal_block_perc))
-            update = (n[-2,:] < N*fixation)  # fixation of fittest genotype with aneuploidy
-            update = update*directmut
+            update = (n[-2,:] < N*fixation)  #fixation of fittest genotype with aneuploidy
+            update = update*directmut  #get all fixed repetitions into update
             #solution to problem? 20.4.22
             combfix[update] = (n[-1,update]+n[-2,update] < N*fixation)  # fixation of fittest genotype as a combination
-            update = update*combfix
+            update = update*combfix     #get all fixed repetitions into update
             
         #last generation update
         p = n/N 
-        if fix_frequ is True:
+        if fix_frequ is True: #Record the last frequencies of the two bins of 2n* if wanted
             P.append(p[3:5])
         nona=(~directmut).nonzero()[0]
         combfix=(~combfix).nonzero()[0]
@@ -210,262 +204,8 @@ class SingleLocusModelExt:
         #return T, nona
     
         
-    # simulations only with waiting time as return
-    def run_simulations_time_fixT(self, N, mutation_rate, aneuploidy_rate, aneuploidy_loss_rate, w1, w2, w3, repetitions=1000, max_gen=2500, fixation=0.95, seed=5,clonal_intf=True):
-        curr_rand_state = np.random.get_state()
-        np.random.seed(seed)
-
-        #0 wt
-        #1 tri
-        #2 tri+
-        #3 +
-        M = np.diag(np.repeat(0.,5))
-        M[0][1] = aneuploidy_rate
-        M[1][2] = mutation_rate
-        M[2][3] = aneuploidy_loss_rate 
-        M[0][4] = self.k*mutation_rate  
-        w = [1., w1, w2, w3, w3]
-        times, non_aneu_fix, fixT= self._simulation_time_fixT(N, w, M, repetitions=repetitions, fixation=fixation, max_gen=max_gen,clonal_intf=clonal_intf)
-        
-        np.random.set_state(curr_rand_state)
-        return (times, non_aneu_fix, fixT)
-
     
-    
-    def _simulation_time_fixT(self, N, w, M ,repetitions=1000, fixation=0.95, max_gen=2500,clonal_intf=True):
-        assert N > 0
-        N = np.uint64(N)
-        L = len(w)
-        S = np.diag(w)
-                                
-        if clonal_intf==False:
-            Mnotr=np.diag(np.repeat(0.,5))
-            Mnotr[1][2]=M[1][2]
-            Mnotr[2][3]=M[2][3]
-            Mnotr=Mnotr.transpose()
-            np.fill_diagonal(Mnotr,0) #neutralling side effect
-            np.fill_diagonal(Mnotr,1-Mnotr.sum(axis=0)) #side effect
-            Enotr= Mnotr @ S                         
-
-        M = M.transpose() 
-        np.fill_diagonal(M,0) #neutralling side effect
-        np.fill_diagonal(M,1-M.sum(axis=0)) #side effect
-
-        E = M @ S
-
-        # rows are genotypes, cols are repretitions
-        n = np.zeros((L, repetitions))
-        n[0,:] = N    
-        # which columns to update
-        update = np.array([True] * repetitions)
-        directmut = np.array([True] * repetitions)
-        combfix=np.array([True] * repetitions)
-        clonal_int=np.array([False]*repetitions)
-        fixbool=np.array([False]*repetitions)
-        # follow time
-        T = np.zeros(repetitions)
-        fixT=np.zeros(repetitions)
-        t = 0
-        # follow population mean fitness
-        W = []
-        P = []
-
-        while update.any():
-            if t>=max_gen-1:
-                break
-            
-            t += 1
-            T[update] = t   
-            
-            fixT[fixbool*update] +=1
-            fixT[~fixbool] =0
-            p = n/N  # counts to frequencies
-            #W.append(w.reshape(1, L) @ p)  # mean fitness
-            #P.append(p)
-            if clonal_intf==False:
-                p[:,~clonal_int] = E @ p[:,~clonal_int]  # natural selection + mutation 
-                p[:,clonal_int] = Enotr @ p[:,clonal_int]     
-            else:
-                p = E @ p
-            #p[-1,clonal_int]=0
-            p /= p.sum(axis=0)  # mean fitness
-            for j in update.nonzero()[0]:
-                # random genetic drift
-                n[:,j] = np.random.multinomial(N, p[:,j])
-            directmut[update] = (n[-1,update] < N*fixation)  # fixation of fittest genotype directly through mutation
-            fixbool[update]=(n[1,update]+n[2,update]+n[3,update]+n[4,update])>=1
-            if clonal_intf==False:
-                clonal_int[update]=(n[1,update]+n[-1,update])>=1
-            #clonal_int[update]=np.logical_and((n[1,update]+n[2,update]>N*clonal_block_perc),(n[-1,update]<N*clonal_block_perc))
-            update = (n[-2,:] < N*fixation)  # fixation of fittest genotype with aneuploidy
-            update = update*directmut
-            #combfix[update] = (n[-1,update]+n[-2,update] < N*fixation)  # fixation of fittest genotype as a combination
-            #update = update*combfix
-        #last generation update
-        p = n/N 
-        #P.append(p)
-        nona=(~directmut).nonzero()[0]
-        #combfix=(~combfix).nonzero()[0]
-
-        #return T, (nona,combfix)
-        return T, nona, fixT
-    
-    
-    # simulations only with waiting time as return
-    def run_simulations_time_hyptest(self, N, mutation_rate, aneuploidy_rate, aneuploidy_loss_rate, w1, w2, w3, repetitions=1000, max_gen=2500, fixation=0.95, seed=5, clonal_aneu=1, clonal_mut=1):
-        curr_rand_state = np.random.get_state()
-        np.random.seed(seed)
-
-        #0 wt
-        #1 tri
-        #2 tri+
-        #3 +
-        M = np.diag(np.repeat(0.,5))
-        M[0][1] = aneuploidy_rate
-        M[1][2] = mutation_rate
-        M[2][3] = aneuploidy_loss_rate 
-        M[0][4] = self.k*mutation_rate  
-        w = [1., w1, w2, w3, w3]
-        times, non_aneu_fix= self._simulation_time_hyptest(N, w, M, repetitions=repetitions, fixation=fixation, max_gen=max_gen, clonal_aneu=1, clonal_mut=1)
-        
-        np.random.set_state(curr_rand_state)
-        return (times, non_aneu_fix)
-
-    
-    #have side effect on M, but not harmfull, can run twice simulation
-    def _simulation_time_hyptest(self, N, w, M ,repetitions=1000, fixation=0.95, max_gen=2500,clonal_aneu=1,clonal_mut=1):
-        assert N > 0
-        N = np.uint64(N)
-        L = len(w)
-        S = np.diag(w)
-
-        M = M.transpose() 
-        np.fill_diagonal(M,0) #neutralling side effect
-        np.fill_diagonal(M,1-M.sum(axis=0)) #side effect
-
-        E = M @ S
-
-        # rows are genotypes, cols are repretitions
-        n = np.zeros((L, repetitions))
-        n[0,:] = N    
-        # which columns to update
-        update = np.array([True] * repetitions)
-        directmut = np.array([True] * repetitions)
-        combfix=np.array([True] * repetitions)
-        clonal_int_aneu=np.array([False]*repetitions)
-        clonal_int_mut=np.array([False]*repetitions)
-        # follow time
-        T = np.zeros(repetitions)
-        t = 0
-        # follow population mean fitness
-        W = []
-        P = []
-
-        while update.any():
-            if t>=max_gen-1:
-                break
-
-            t += 1
-            T[update] = t        
-            p = n/N  # counts to frequencies
-    #         W.append(w.reshape(1, L) @ p)  # mean fitness
-            #P.append(p)
-            p = E @ p  # natural selection + mutation        
-            p[-1,clonal_int_aneu]=0
-            p[1,clonal_int_mut]=0
-            p /= p.sum(axis=0)  # mean fitness
-            for j in update.nonzero()[0]:
-                # random genetic drift
-                n[:,j] = np.random.multinomial(N, p[:,j])
-            directmut[update] = (n[-1,update] < N*fixation)  # fixation of fittest genotype directly through mutation
-            #clonal_int[update]=(n[1,update]+n[2,update]>N*clonal_block_perc & n[-1,update]<N*clonal_block_perc)
-            clonal_int_aneu[update]=(n[1,update]+n[2,update]>=clonal_aneu)
-            clonal_int_mut[update]=(n[-1,update]>=clonal_mut)
-            update = (n[-2,:] < N*fixation)  # fixation of fittest genotype with aneuploidy
-            update = update*directmut
-            #combfix[update] = (n[-1,update]+n[-2,update] < N*fixation)  # fixation of fittest genotype as a combination
-            #update = update*combfix
-        #last generation update
-        p = n/N 
-        #P.append(p)
-        nona=(~directmut).nonzero()[0]
-        #combfix=(~combfix).nonzero()[0]
-
-        #return T, (nona,combfix)
-        return T, nona
-    
-    
-    
-    
-    
-    def meandiffT(self, fullarr, diffT):
-        means0 = []
-        means1 = []
-        for j in range(len(fullarr)):
-            update = np.array([True] * len(fullarr[j]))
-            update[diffT[j]] = False
-            if update.any(): 
-                m0=np.mean(fullarr[j][update])
-            else:
-                m0=0    
-            m1=np.mean(fullarr[j][~update])
-            means0.append(m0)
-            means1.append(m1)
-        return (means0, means1)
-    
-    def meanstddiffT(self, fullarr, diffT):
-        means0 = []
-        means1 = []
-        std0 = []
-        std1 = []
-        for j in range(len(fullarr)):
-            update = np.array([True] * len(fullarr[j]))
-            update[diffT[j]] = False
-            if update.any(): 
-                m0=np.mean(fullarr[j][update])
-                sd0=np.std(fullarr[j][update])
-            else:
-                m0=0 
-                sd0=0
-            if (~update).any():
-                m1=np.mean(fullarr[j][~update])
-                sd1=np.std(fullarr[j][~update])
-            else:
-                m1=0
-                sd1=0
-            means0.append(m0)
-            means1.append(m1)
-            std0.append(sd0)
-            std1.append(sd1)
-        return (means0, means1), (std0,std1)
-
-    def meanpercerrdiffT(self, fullarr, diffT):
-        means_aneu = []
-        means_mut = []
-        err0595_aneu = []
-        err0595_mut = []
-        for j in range(len(fullarr)):
-            update = np.array([True] * len(fullarr[j]))
-            update[diffT[j]] = False
-            if update.any(): 
-                m0=np.mean(fullarr[j][update])
-                e0=[m0-np.percentile(fullarr[j][update],5),np.percentile(fullarr[j][update],95)-m0]
-            else:
-                m0=0 
-                e0=[0,0]
-            if (~update).any():
-                m1=np.mean(fullarr[j][~update])
-                e1=[m1-np.percentile(fullarr[j][~update],5),np.percentile(fullarr[j][~update],95)-m1]
-            else:
-                m1=0
-                e1=[0,0]
-            means_aneu.append(m0)
-            means_mut.append(m1)
-            err0595_aneu.append(e0)
-            err0595_mut.append(e1)
-        return (means_aneu, means_mut), (np.array(err0595_aneu).transpose(),np.array(err0595_mut).transpose())
-
-    
+    #Plot the frequency trajectories of the simulations
     def plot_progress(self, nparr, replicaid, state_names, colors, fixation = 0.95, ax=None, alpha=1, legend=True, xlim=True, ylim=(0,1), lw=None, g_o=False, logscale=False):
         if ax is None:
             fig, ax = plt.subplots(figsize=(14,5))
@@ -530,25 +270,4 @@ class SingleLocusModelExt:
         return ax
        # plt.show()
         
-        
-        
-        
-        
-        
-        
-#        if legend is True:
-#            ind = -1
-#            for n,c in zip(state_names, colors):    
-#                ind+=1
-#                progress = nparr[:,ind][:last,replicaid]
-
-#                if g_o is False:
-#                    ax.plot(range(last), progress, label=n, color=c, alpha=alpha, lw=lw) 
-#                elif ind==3:
-##                    ax.plot(range(last), progress, label=n, color=c, alpha=.4, lw=lw, zorder=4)
-#                elif ind==2:
-#                    ax.plot(range(last), progress, label=n, color=c, alpha=alpha, lw=lw, zorder=5)
-#                else:
-#                    ax.plot(range(last), progress, label=n, color=c, alpha=alpha, lw=lw)
       
-    
